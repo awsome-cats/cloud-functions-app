071d55b9af20d81834d7401ea26e4f95d0ed9841
awsome-cats <cattwin82@gmail.com>
2020-06-11 19:36:50 +0900

    set register & login form

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+const requestModal = document.querySelector('.new-request');
+const requestLink = document.querySelector('.add-request');
+
+// open request modal
+requestLink.addEventListener('click', () => {
+  requestModal.classList.add('open');
+});
+
+// close request modal
+requestModal.addEventListener('click', (e) => {
+  if (e.target.classList.contains('new-request')) {
+    requestModal.classList.remove('open');
+  }
+});
+
+// fuctions/index.jsのsayHello functionを呼び出して、イベントにする
+const button = document.querySelector('.call');
+button.addEventListener('click', () => {
+  // get function reference
+
+  const sayHello = firebase.functions().httpsCallable('sayHello')
+  sayHello({ name: 'Miyuki'}).then(result => {
+    console.log(result.data)
+  })
+})
+
+// functionsのsayHello(data, context)  <------ アクセス
